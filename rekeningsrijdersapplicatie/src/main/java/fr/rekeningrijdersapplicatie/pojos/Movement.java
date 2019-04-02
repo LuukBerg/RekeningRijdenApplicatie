5ba667f1daf20cb97f2a25ef997fa122b102b00b
Grinsven,Kennard K.C.H. van <kennard.vangrinsven@student.fontys.nl>
2019-04-02 10:31:49 +0200

    Month klasse verwijderd. java.time.Month gebruikt.

--- rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/pojos/Movement.java
+++ rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/pojos/Movement.java
@@ -1,6 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package fr.rekeningrijdersapplicatie.pojos;
 
+/**
+ *
+ * @author Ken
+ */
 public class Movement {
+
     private long id;
     private double lat;
     private double lon;
