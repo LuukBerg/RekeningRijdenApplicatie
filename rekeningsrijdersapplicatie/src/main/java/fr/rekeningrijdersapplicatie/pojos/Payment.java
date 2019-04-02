bf3d19d0dd286150ab242044536843d7d8b838da
Grinsven,Kennard K.C.H. van <kennard.vangrinsven@student.fontys.nl>
2019-04-02 14:31:24 +0200

    JPA annotaties toegevoegd.

--- rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/pojos/Payment.java
+++ rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/pojos/Payment.java
@@ -1,7 +1,20 @@
 package fr.rekeningrijdersapplicatie.pojos;
 
-public class Payment {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Payment implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    
+    @Column(nullable = false, unique = true)
     private String invoiceUuid;
 
     public long getId() {
