e383ae52e353a3671ecc6599d1985ca28865044f
Bkhiti,Rabie R <r.bkhiti@student.fontys.nl>
2019-04-02 09:08:08 +0200

    Added interface functions.

--- rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/dao/interfaces/IInvoiceDAO.java
+++ rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/dao/interfaces/IInvoiceDAO.java
@@ -1,4 +1,9 @@
 package fr.rekeningrijdersapplicatie.dao.interfaces;
 
+import fr.rekeningrijdersapplicatie.pojos.Invoice;
+
+import java.util.List;
+
 public interface IInvoiceDAO {
+    List<Invoice> getInvoices();
 }
--- rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/dao/interfaces/IUserDAO.java
+++ rekeningsrijdersapplicatie/src/main/java/fr/rekeningrijdersapplicatie/dao/interfaces/IUserDAO.java
@@ -3,10 +3,14 @@ package fr.rekeningrijdersapplicatie.dao.interfaces;
 import fr.rekeningrijdersapplicatie.pojos.LoginInfo;
 import fr.rekeningrijdersapplicatie.pojos.RegistrationInfo;
 import fr.rekeningrijdersapplicatie.pojos.User;
+import fr.rekeningrijdersapplicatie.pojos.Vehicle;
+
+import java.util.List;
 
 public interface IUserDAO {
     User register(RegistrationInfo registrationInfo);
 
     User login(LoginInfo loginInfo);
 
+    List<Vehicle> getVehicles();
 }
